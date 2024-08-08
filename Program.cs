5beee4fc9f3603e01e86348f5169b1b61ea8327f
Omer Catalbas <78023319+omrctlbs@users.noreply.github.com>
2024-08-08 16:25:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Finding the largest of the three numbers entered
+
+Console.WriteLine("Enter the first number:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the second number:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the third number:");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+
+if (number1 > number2 && number1 > number3)
+{
+    Console.WriteLine("The first number is the largest.");
+}
+else if (number2 > number1 && number2 > number3)
+{
+    Console.WriteLine("The second number is the largest.");
+}
+else if (number3 > number1 && number3 > number2)
+{
+    Console.WriteLine("The third number is the largest.");
+}
+else
+{
+    Console.WriteLine("Please enter a valid value.");
+}
